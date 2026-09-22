03f456c32628e7d8852f7cef21b059aa5cf005a7
Claus Stadler <RavenArkadon@googlemail.com>
2026-09-22 22:20:26 +0200

    simplified namespace

--- jena-inf-light/src/main/java/org/aksw/jena/inf/init/JenaInfLightTerms.java
+++ jena-inf-light/src/main/java/org/aksw/jena/inf/init/JenaInfLightTerms.java
@@ -1,5 +1,5 @@
 package org.aksw.jena.inf.init;
 
 public class JenaInfLightTerms {
-    public static final String NS = "https://w3id.org/aksw/jena/inf#";
+    public static final String NS = "https://w3id.org/aksw/jena#";
 }
